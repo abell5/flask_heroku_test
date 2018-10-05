ef625283f794ccfca372d3b1339630f017f225af
Andrew Bell <abell5@g.clemson.edu>
2018-10-05 17:00:37 -0400

    Add template pages to app

--- app.py
+++ app.py
@@ -1,8 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template, flash, request, url_for, redirect, session
+import sys
 
 app = Flask(__name__)
+app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
 
 @app.route("/")
-def index():
-    return 'Hello_world!'
-	#return render_template('home.html')
+def home():
+    return render_template('home.html')
+	
+if __name__ == '__main__':
+    app.debug = True
+    app.run()
